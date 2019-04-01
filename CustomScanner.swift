a6836aab8b72d470fb064a0647cd03815692f5d3
Gbemiro Jiboye <gbenroscience@yahoo.com>
2019-04-01 12:03:54 +0100

    Add files via upload

--- CustomScanner.swift
+++ CustomScanner.swift
@@ -1,4 +1,5 @@
 
+
 //
 //  CustomScanner.swift
 //  Kalculitzer
@@ -10,7 +11,9 @@
 import Foundation
 
 
-struct CustomScanner{
+
+
+public struct CustomScanner{
     /**
      * The input to be scanned into tokens.
      */
@@ -28,7 +31,7 @@ struct CustomScanner{
     
     
     
-
+    
     /**
      *
      * @param input The input to scan.
@@ -81,19 +84,19 @@ struct CustomScanner{
             
             for token in tokens {
                 
-              let len = token.count
+                let len = token.count
                 
                 if (len > 0 && i + len <= inp.count) {
-                    let portion = substring(text: inp,start:  i, end: i + len);
+                    let portion = substring(text: inp,start:  i, end: i + len)
                     
                     if (portion == token) {
                         if (i != 0) {//avoid empty spaces
-                            parse.append(substring(text: inp, start: 0, end: i));
+                            parse.append(substring(text: inp, start: 0, end: i))
                         }
                         if (includeTokensInOutput) {
                             parse.append(token);
                         }
-                        inp = substring( text: inp, startToEnd: i + len);
+                        inp = substring( text: inp, startToEnd: i + len)
                         i = -1;
                         break;
                     }
@@ -102,43 +105,81 @@ struct CustomScanner{
                 
             }
             
-                i+=1
+            i+=1
         }
+        
+        if (!inp.isEmpty) {
+            parse.append(inp)
+        }
+        
         
         return parse
     }
     
+    
+    public func runStuff(){
+        
+        var tokens: [String] = ["sin","sinh","cos","+","(",")"]
+        
+        let scanner = CustomScanner(input: "42sin(A)cos(B)+9sin(A)sinh(B)", includeTokensInOutput: false, tokens: tokens)
+        
+        
+        let start = Int(DispatchTime.now().rawValue)
+        
+        var i = 0
+        
+        let MAX_ITERS: Int = 10
+        
+        
+        while i < MAX_ITERS {
+            scanner.scan()
+            i += 1
+        }
+        
+        var end = Int(DispatchTime.now().rawValue)
+        
+        
+        var duration = end - start
+        
+        print(
+            "Benchmarks: Total time to run \(MAX_ITERS) iterations is \(duration)\n Average time to run each function is \(  (duration/MAX_ITERS)  )" )
+        
+    }
+    
 }
-    
-    
-    private func lengthWise(value1: String, value2: String) -> Bool {
-        // One string is alphabetically first.
-        // ... True means value1 precedes value2.
-        return value2.count > value1.count;
+
+
+private func lengthWise(value1: String, value2: String) -> Bool {
+    // One string is alphabetically first.
+    // ... True means value1 precedes value2.
+    return value2.count < value1.count;
+}
+
+func substring(text: String, start:Int, end:Int) -> String{
+    if (start < 0 || start > text.count) || (end < 0 || end > text.count) || start > end {
+        return ""
     }
+    let startIndex = text.index(text.startIndex, offsetBy: start)
+    let endIndex = text.index(text.startIndex, offsetBy: end)
     
-    func substring(text: String, start:Int, end:Int) -> String{
-        if (start < 0 || start > text.count) || (end < 0 || end > text.count) || start > end {
-            return ""
-        }
-        let startIndex = text.index(text.startIndex, offsetBy: start)
-        let endIndex = text.index(text.startIndex, offsetBy: end)
-        
-        return String(text[startIndex..<endIndex])
-    }
+    return String(text[startIndex..<endIndex])
+}
+
+/**
+ * This returns an empty string in case of index errors
+ * Else it returns all characters from the supplied index
+ * to the end of the string
+ */
+func substring(text: String, startToEnd:Int ) -> String{
     
-    /**
-     * This returns an empty string in case of index errors
-     * Else it returns all characters from the supplied index
-     * to the end of the string
-     */
-     func substring(text: String, startToEnd:Int ) -> String{
-        
-        if startToEnd < 0 || startToEnd > text.count {
-            return ""
-        }
-        let startIndex = text.index(text.startIndex, offsetBy: startToEnd)
-        let endIndex = text.endIndex
-        
-        return String(text[startIndex..<endIndex])
+    if startToEnd < 0 || startToEnd > text.count {
+        return ""
     }
+    let startIndex = text.index(text.startIndex, offsetBy: startToEnd)
+    let endIndex = text.endIndex
+    
+    return String(text[startIndex..<endIndex])
+}
+
+
+
